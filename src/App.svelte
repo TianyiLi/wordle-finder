570184614a46ecdbe93e01450a464387e8c56455
TianyiLi <e0991100238@gmail.com>
2022-03-15 23:25:12 +0800

    add answer getter

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,30 @@
   let masks = [{ id: 0, mask: "" }];
   let containsStr = "";
 
+  let dangerousToggle = false;
+
+  function predict(e: string | Date) {
+    const _date = new Date(e);
+    const Ga = new Date(2021, 5, 19, 0, 0, 0, 0);
+    function Ba(e: string | Date, a: string | Date) {
+      var s = new Date(e),
+        t = new Date(a).setHours(0, 0, 0, 0) - s.setHours(0, 0, 0, 0);
+      return Math.round(t / 864e5);
+    }
+    function Va(e: string | Date) {
+      var a,
+        s = Ba(Ga, e);
+      return (a = s % key.length), key[a];
+    }
+    return Va(_date);
+  }
+
   const sampleObj = {
     includeStr,
     excludeStr,
     masks,
-    containsStr
-  }
+    containsStr,
+  };
 
   function addMask() {
     masks = produce(masks, (draft) => {
@@ -35,12 +53,19 @@
 
   function clickGetGoogleTranslate(str: string) {
     const url = `https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=${str}`;
-    window.open(url, "_blank", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=600");
+    window.open(
+      url,
+      "_blank",
+      "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=600"
+    );
   }
 
-  function getResult(
-    { masks, includeStr, excludeStr, containsStr } = sampleObj
-  ) {
+  function getResult({
+    masks,
+    includeStr,
+    excludeStr,
+    containsStr,
+  } = sampleObj) {
     let maskCond = masks.filter(
       (m) => /^([a-zA-Z\_\!]+)$/.test(m.mask) && m.mask.length >= 5
     );
@@ -62,32 +87,31 @@
     if (!cond.some((b) => b)) {
       return "";
     }
-    const result = key
-      .filter((l) => {
-        if (cond[0]) {
-          if (!_includeStr.every((c) => ~l.indexOf(c))) {
-            return false;
-          }
+    const result = key.filter((l) => {
+      if (cond[0]) {
+        if (!_includeStr.every((c) => ~l.indexOf(c))) {
+          return false;
         }
-        if (cond[1]) {
-          if (_excludeStr.test(l)) {
-            _excludeStr.lastIndex = 0;
-            return false;
-          }
+      }
+      if (cond[1]) {
+        if (_excludeStr.test(l)) {
+          _excludeStr.lastIndex = 0;
+          return false;
         }
-        if (cond[2]) {
-          if (!_maskTest.every((m) => m.test(l))) {
-            _maskTest.forEach((m) => (m.lastIndex = 0));
-            return false;
-          }
+      }
+      if (cond[2]) {
+        if (!_maskTest.every((m) => m.test(l))) {
+          _maskTest.forEach((m) => (m.lastIndex = 0));
+          return false;
         }
-        if (cond[3]) {
-          if (!~l.toLowerCase().indexOf(containsStr.toLowerCase())) {
-            return false;
-          }
+      }
+      if (cond[3]) {
+        if (!~l.toLowerCase().indexOf(containsStr.toLowerCase())) {
+          return false;
         }
-        return true;
-      }, [] as string[])
+      }
+      return true;
+    }, [] as string[]);
     return result;
   }
 
@@ -101,6 +125,14 @@
 
 <main>
   <div class="main">
+    <div class="section">
+      <label for="Dangerous warning">Get predicted wordle answer</label>
+      <input
+        type="checkbox"
+        id="Dangerous warning"
+        on:change={() => (dangerousToggle = !dangerousToggle)}
+      />
+    </div>
     <div class="section">
       <label for="include">Include</label>
       <input id="include" type="text" bind:value={includeStr} />
@@ -141,6 +173,11 @@
         <br />
       {/each}
     </div>
+    {#if dangerousToggle}
+      <div class="section">
+        {predict(new Date())}
+      </div>
+    {/if}
     <div class="section">
       <label for="frame"> open worldle </label>
       <button id="frame" on:click={openWordle}>open</button>
@@ -148,7 +185,11 @@
 
     <div class="result-section">
       {#each result as str}
-        <a target="_blank" href={`https://translate.google.com/#view=home&op=translate&sl=en&tl=zh-TW&text=${str}`}>{str}</a>, 
+        <a
+          target="_blank"
+          href={`https://translate.google.com/#view=home&op=translate&sl=en&tl=zh-TW&text=${str}`}
+          >{str}</a
+        >,
       {/each}
     </div>
   </div>
