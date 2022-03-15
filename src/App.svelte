6b661c2c069993bdd0935421abeb87d04bf9ba3b
TianyiLi <e0991100238@gmail.com>
2022-03-15 23:02:55 +0800

    add contains, translate

--- src/App.svelte
+++ src/App.svelte
@@ -1,62 +1,102 @@
 <script lang="ts">
-  import { key } from './constant'
-  let includeStr = ''
-  let excludeStr = ''
-  let masks = [{ id: 0, mask: '' }]
+  import { key } from "./constant";
+  import { produce } from "immer";
+  let includeStr = "";
+  let excludeStr = "";
+  let masks = [{ id: 0, mask: "" }];
+  let containsStr = "";
+
+  const sampleObj = {
+    includeStr,
+    excludeStr,
+    masks,
+    containsStr
+  }
+
+  function addMask() {
+    masks = produce(masks, (draft) => {
+      draft.push({ id: masks.length, mask: "" });
+    });
+  }
+
+  function removeMask(id: number) {
+    masks = produce(masks, (draft) => {
+      draft.splice(id, 1);
+    });
+  }
 
   function openWordle() {
     window.open(
-      'https://www.nytimes.com/games/wordle/index.html',
-      '_blank',
-      'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=600'
-    )
+      "https://www.nytimes.com/games/wordle/index.html",
+      "_blank",
+      "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=600"
+    );
   }
 
-  $: result = (() => {
+  function clickGetGoogleTranslate(str: string) {
+    const url = `https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=${str}`;
+    window.open(url, "_blank", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=600");
+  }
+
+  function getResult(
+    { masks, includeStr, excludeStr, containsStr } = sampleObj
+  ) {
     let maskCond = masks.filter(
       (m) => /^([a-zA-Z\_\!]+)$/.test(m.mask) && m.mask.length >= 5
-    )
+    );
 
-    const cond = [!!includeStr, !!excludeStr, maskCond.length]
-    const _includeStr = includeStr.toLowerCase().split('')
-    const _excludeStr = new RegExp(excludeStr.toLowerCase().split('').join('|'))
+    const cond = [!!includeStr, !!excludeStr, maskCond.length, !!containsStr];
+    const _includeStr = includeStr.toLowerCase().split("");
+    const _excludeStr = new RegExp(
+      excludeStr.toLowerCase().split("").join("|")
+    );
     const _maskTest = maskCond.map((m) => {
       return new RegExp(
         m.mask
           .toLowerCase()
-          .replace(/\_/g, '.')
-          .replace(/\!(\w)/g, '[^$1]')
-      )
-    })
+          .replace(/\_/g, ".")
+          .replace(/\!(\w)/g, "[^$1]")
+      );
+    });
 
     if (!cond.some((b) => b)) {
-      return ''
+      return "";
     }
     const result = key
       .filter((l) => {
         if (cond[0]) {
           if (!_includeStr.every((c) => ~l.indexOf(c))) {
-            return false
+            return false;
           }
         }
         if (cond[1]) {
           if (_excludeStr.test(l)) {
-            _excludeStr.lastIndex = 0
-            return false
+            _excludeStr.lastIndex = 0;
+            return false;
           }
         }
         if (cond[2]) {
           if (!_maskTest.every((m) => m.test(l))) {
-            _maskTest.forEach((m) => (m.lastIndex = 0))
-            return false
+            _maskTest.forEach((m) => (m.lastIndex = 0));
+            return false;
           }
         }
-        return true
+        if (cond[3]) {
+          if (!~l.toLowerCase().indexOf(containsStr.toLowerCase())) {
+            return false;
+          }
+        }
+        return true;
       }, [] as string[])
-      .join()
-    console.log(result)
-    return result
-  })()
+    return result;
+  }
+
+  $: result = getResult({
+    includeStr,
+    excludeStr,
+    masks,
+    containsStr,
+  });
 </script>
 
 <main>
@@ -70,24 +110,35 @@
       <input id="exclude" type="text" bind:value={excludeStr} />
     </div>
     <div class="section">
+      <label for="contains">contains</label>
+      <input id="contains" type="text" bind:value={containsStr} />
+    </div>
+    <div class="section">
       <label for="mask"
-        >Mask (empty by _, exclude with ! prefix) <button
-          on:click={() => {
-            masks = [...masks, { id: masks.length, mask: '' }]
-          }}>add candidate option</button
+        >Mask (empty by _, exclude with ! prefix) <button on:click={addMask}
+          >add candidate option</button
         ></label
       >
       <br />
       {#each masks as mask, index}
         <input
           type="text"
-          style="margin-bottom: 10px"
+          style="margin-bottom: 10px; margin-right: 10px"
           bind:value={mask.mask}
           on:input={(e) => {
-            masks[index].mask = e.target.value
+            masks[index].mask = e.target.value;
           }}
         />
-        <br/>
+        {#if masks.length !== 1}
+          <button
+            on:click={() => {
+              removeMask(index);
+            }}
+          >
+            remove
+          </button>
+        {/if}
+        <br />
       {/each}
     </div>
     <div class="section">
@@ -96,15 +147,17 @@
     </div>
 
     <div class="result-section">
-      {result}
+      {#each result as str}
+        <a target="_blank" href={`https://translate.google.com/#view=home&op=translate&sl=en&tl=zh-TW&text=${str}`}>{str}</a>, 
+      {/each}
     </div>
   </div>
 </main>
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 
   main {
